52e5188a329abc407a0423f1441a841017f65973
Jeff Hykin <jeffhykin@Jeffs-iMac.local>
2019-04-21 01:53:20 -0500

    _fix z index

--- index.js
+++ index.js
@@ -9,111 +9,109 @@ let opacityRandomness = () => {
     return Math.ceil(output)
 }
 
-
-// wait for body to exist
-window.addEventListener('load', () => {
+let onceBodyExists = () => {
     var sheet = document.createElement('style')
     sheet.innerHTML = `
-            .opacity1 { opacity: .01; }
-            .opacity2 { opacity: .02; }
-            .opacity3 { opacity: .03; }
-            .opacity4 { opacity: .04; }
-            .opacity5 { opacity: .05; }
-            .opacity6 { opacity: .06; }
-            .opacity7 { opacity: .07; }
-            .opacity8 { opacity: .08; }
-            .opacity9 { opacity: .09; }
-            .opacity10 { opacity: .10; }
-            .opacity11 { opacity: .11; }
-            .opacity12 { opacity: .12; }
-            .opacity13 { opacity: .13; }
-            .opacity14 { opacity: .14; }
-            .opacity15 { opacity: .15; }
-            .opacity16 { opacity: .16; }
-            .opacity17 { opacity: .17; }
-            .opacity18 { opacity: .18; }
-            .opacity19 { opacity: .19; }
-            .opacity20 { opacity: .20; }
-            .opacity21 { opacity: .21; }
-            .opacity22 { opacity: .22; }
-            .opacity23 { opacity: .23; }
-            .opacity24 { opacity: .24; }
-            .opacity25 { opacity: .25; }
-            .opacity26 { opacity: .26; }
-            .opacity27 { opacity: .27; }
-            .opacity28 { opacity: .28; }
-            .opacity29 { opacity: .29; }
-            .opacity30 { opacity: .30; }
-            .opacity31 { opacity: .31; }
-            .opacity32 { opacity: .32; }
-            .opacity33 { opacity: .33; }
-            .opacity34 { opacity: .34; }
-            .opacity35 { opacity: .35; }
-            .opacity36 { opacity: .36; }
-            .opacity37 { opacity: .37; }
-            .opacity38 { opacity: .38; }
-            .opacity39 { opacity: .39; }
-            .opacity40 { opacity: .40; }
-            .opacity41 { opacity: .41; }
-            .opacity42 { opacity: .42; }
-            .opacity43 { opacity: .43; }
-            .opacity44 { opacity: .44; }
-            .opacity45 { opacity: .45; }
-            .opacity46 { opacity: .46; }
-            .opacity47 { opacity: .47; }
-            .opacity48 { opacity: .48; }
-            .opacity49 { opacity: .49; }
-            .opacity50 { opacity: .50; }
-            .opacity51 { opacity: .51; }
-            .opacity52 { opacity: .52; }
-            .opacity53 { opacity: .53; }
-            .opacity54 { opacity: .54; }
-            .opacity55 { opacity: .55; }
-            .opacity56 { opacity: .56; }
-            .opacity57 { opacity: .57; }
-            .opacity58 { opacity: .58; }
-            .opacity59 { opacity: .59; }
-            .opacity60 { opacity: .60; }
-            .opacity61 { opacity: .61; }
-            .opacity62 { opacity: .62; }
-            .opacity63 { opacity: .63; }
-            .opacity64 { opacity: .64; }
-            .opacity65 { opacity: .65; }
-            .opacity66 { opacity: .66; }
-            .opacity67 { opacity: .67; }
-            .opacity68 { opacity: .68; }
-            .opacity69 { opacity: .69; }
-            .opacity70 { opacity: .70; }
-            .opacity71 { opacity: .71; }
-            .opacity72 { opacity: .72; }
-            .opacity73 { opacity: .73; }
-            .opacity74 { opacity: .74; }
-            .opacity75 { opacity: .75; }
-            .opacity76 { opacity: .76; }
-            .opacity77 { opacity: .77; }
-            .opacity78 { opacity: .78; }
-            .opacity79 { opacity: .79; }
-            .opacity80 { opacity: .80; }
-            .opacity81 { opacity: .81; }
-            .opacity82 { opacity: .82; }
-            .opacity83 { opacity: .83; }
-            .opacity84 { opacity: .84; }
-            .opacity85 { opacity: .85; }
-            .opacity86 { opacity: .86; }
-            .opacity87 { opacity: .87; }
-            .opacity88 { opacity: .88; }
-            .opacity89 { opacity: .89; }
-            .opacity90 { opacity: .90; }
-            .opacity91 { opacity: .91; }
-            .opacity92 { opacity: .92; }
-            .opacity93 { opacity: .93; }
-            .opacity94 { opacity: .94; }
-            .opacity95 { opacity: .95; }
-            .opacity96 { opacity: .96; }
-            .opacity97 { opacity: .97; }
-            .opacity98 { opacity: .98; }
-            .opacity99 { opacity: .99; }
-            .opacity100 { opacity: 1; }
+            .purple-tile-opacity1 { opacity: .01; }
+            .purple-tile-opacity2 { opacity: .02; }
+            .purple-tile-opacity3 { opacity: .03; }
+            .purple-tile-opacity4 { opacity: .04; }
+            .purple-tile-opacity5 { opacity: .05; }
+            .purple-tile-opacity6 { opacity: .06; }
+            .purple-tile-opacity7 { opacity: .07; }
+            .purple-tile-opacity8 { opacity: .08; }
+            .purple-tile-opacity9 { opacity: .09; }
+            .purple-tile-opacity10 { opacity: .10; }
+            .purple-tile-opacity11 { opacity: .11; }
+            .purple-tile-opacity12 { opacity: .12; }
+            .purple-tile-opacity13 { opacity: .13; }
+            .purple-tile-opacity14 { opacity: .14; }
+            .purple-tile-opacity15 { opacity: .15; }
+            .purple-tile-opacity16 { opacity: .16; }
+            .purple-tile-opacity17 { opacity: .17; }
+            .purple-tile-opacity18 { opacity: .18; }
+            .purple-tile-opacity19 { opacity: .19; }
+            .purple-tile-opacity20 { opacity: .20; }
+            .purple-tile-opacity21 { opacity: .21; }
+            .purple-tile-opacity22 { opacity: .22; }
+            .purple-tile-opacity23 { opacity: .23; }
+            .purple-tile-opacity24 { opacity: .24; }
+            .purple-tile-opacity25 { opacity: .25; }
+            .purple-tile-opacity26 { opacity: .26; }
+            .purple-tile-opacity27 { opacity: .27; }
+            .purple-tile-opacity28 { opacity: .28; }
+            .purple-tile-opacity29 { opacity: .29; }
+            .purple-tile-opacity30 { opacity: .30; }
+            .purple-tile-opacity31 { opacity: .31; }
+            .purple-tile-opacity32 { opacity: .32; }
+            .purple-tile-opacity33 { opacity: .33; }
+            .purple-tile-opacity34 { opacity: .34; }
+            .purple-tile-opacity35 { opacity: .35; }
+            .purple-tile-opacity36 { opacity: .36; }
+            .purple-tile-opacity37 { opacity: .37; }
+            .purple-tile-opacity38 { opacity: .38; }
+            .purple-tile-opacity39 { opacity: .39; }
+            .purple-tile-opacity40 { opacity: .40; }
+            .purple-tile-opacity41 { opacity: .41; }
+            .purple-tile-opacity42 { opacity: .42; }
+            .purple-tile-opacity43 { opacity: .43; }
+            .purple-tile-opacity44 { opacity: .44; }
+            .purple-tile-opacity45 { opacity: .45; }
+            .purple-tile-opacity46 { opacity: .46; }
+            .purple-tile-opacity47 { opacity: .47; }
+            .purple-tile-opacity48 { opacity: .48; }
+            .purple-tile-opacity49 { opacity: .49; }
+            .purple-tile-opacity50 { opacity: .50; }
+            .purple-tile-opacity51 { opacity: .51; }
+            .purple-tile-opacity52 { opacity: .52; }
+            .purple-tile-opacity53 { opacity: .53; }
+            .purple-tile-opacity54 { opacity: .54; }
+            .purple-tile-opacity55 { opacity: .55; }
+            .purple-tile-opacity56 { opacity: .56; }
+            .purple-tile-opacity57 { opacity: .57; }
+            .purple-tile-opacity58 { opacity: .58; }
+            .purple-tile-opacity59 { opacity: .59; }
+            .purple-tile-opacity60 { opacity: .60; }
+            .purple-tile-opacity61 { opacity: .61; }
+            .purple-tile-opacity62 { opacity: .62; }
+            .purple-tile-opacity63 { opacity: .63; }
+            .purple-tile-opacity64 { opacity: .64; }
+            .purple-tile-opacity65 { opacity: .65; }
+            .purple-tile-opacity66 { opacity: .66; }
+            .purple-tile-opacity67 { opacity: .67; }
+            .purple-tile-opacity68 { opacity: .68; }
+            .purple-tile-opacity69 { opacity: .69; }
+            .purple-tile-opacity70 { opacity: .70; }
+            .purple-tile-opacity71 { opacity: .71; }
+            .purple-tile-opacity72 { opacity: .72; }
+            .purple-tile-opacity73 { opacity: .73; }
+            .purple-tile-opacity74 { opacity: .74; }
+            .purple-tile-opacity75 { opacity: .75; }
+            .purple-tile-opacity76 { opacity: .76; }
+            .purple-tile-opacity77 { opacity: .77; }
+            .purple-tile-opacity78 { opacity: .78; }
+            .purple-tile-opacity79 { opacity: .79; }
+            .purple-tile-opacity80 { opacity: .80; }
+            .purple-tile-opacity81 { opacity: .81; }
+            .purple-tile-opacity82 { opacity: .82; }
+            .purple-tile-opacity83 { opacity: .83; }
+            .purple-tile-opacity84 { opacity: .84; }
+            .purple-tile-opacity85 { opacity: .85; }
+            .purple-tile-opacity86 { opacity: .86; }
+            .purple-tile-opacity87 { opacity: .87; }
+            .purple-tile-opacity88 { opacity: .88; }
+            .purple-tile-opacity89 { opacity: .89; }
+            .purple-tile-opacity90 { opacity: .90; }
+            .purple-tile-opacity91 { opacity: .91; }
+            .purple-tile-opacity92 { opacity: .92; }
+            .purple-tile-opacity93 { opacity: .93; }
+            .purple-tile-opacity94 { opacity: .94; }
+            .purple-tile-opacity95 { opacity: .95; }
+            .purple-tile-opacity96 { opacity: .96; }
+            .purple-tile-opacity97 { opacity: .97; }
+            .purple-tile-opacity98 { opacity: .98; }
+            .purple-tile-opacity99 { opacity: .99; }
+            .purple-tile-opacity100 { opacity: 1; }
         `
     document.body.appendChild(sheet)
 
@@ -125,6 +123,7 @@ window.addEventListener('load', () => {
     boxContainer.style.position = 'fixed'
     boxContainer.style.top = '0'
     boxContainer.style.left = '0'
+    boxContainer.style.zIndex = '-1'
     boxContainer.style.display = 'flex'
     boxContainer.style.flexDirection = 'row'
     boxContainer.style.flexWrap = 'wrap'
@@ -137,17 +136,28 @@ window.addEventListener('load', () => {
         box.style.backgroundColor = `#9561e2`
         box.style.margin = '0.1rem'
         box.style.transition = `all ${rateOfChange}ms ease-in`
-        box.className = `opacity${opacityRandomness()}`
+        box.className = `purple-tile-opacity${opacityRandomness()}`
         boxContainer.appendChild(box)
     }
 
-    document.body.insertAdjacentElement('afterbegin', boxContainer)
+    document.body.appendChild( boxContainer)
 
 
     let amountOfChange = 15
     setInterval(() => {
         let whichBox = Math.ceil(Math.random() * (numberOfBoxes - 1))
-        let whichOpacity = `opacity${opacityRandomness()}`
+        let whichOpacity = `purple-tile-opacity${opacityRandomness()}`
         boxContainer.childNodes[whichBox].className = whichOpacity
     }, 10);
-}, false)
+}
+
+
+// 
+// load it at the right time
+// 
+if (document.body) {
+    onceBodyExists()
+} else {
+    // wait for body to exist
+    window.addEventListener('load', onceBodyExists, false)
+}
